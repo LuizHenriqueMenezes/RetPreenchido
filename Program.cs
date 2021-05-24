98447dd4d531a05a89b1b4332c8aa72378d71681
Luiz Henrique <lmenezesmessias@gmail.com>
2021-05-24 16:18:25 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int largura, altura;  
+
+            Console.WriteLine("Tamanho do retângulo (digite apenas números de 1 a 10):");
+
+            Console.Write("Largura: ");
+            largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Altura: ");
+            altura = Convert.ToInt32(Console.ReadLine());
+
+           if (largura > 10 || altura > 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("Por favor, recomeçe e digite números de 1 a 10. ");
+                Console.ResetColor();
+                Environment.Exit(-1);
+            }
+        
+            int linha = 1; 
+            while(linha <= altura)
+            {
+                int coluna = 1;
+                while (coluna <= largura)
+                {
+                    Console.Write("* ");
+                    coluna++; 
+                }
+                Console.WriteLine();
+                linha++; 
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RetPreenchido
+
 Exiba um retângulo preenchido, com altura e largura digitados pelo usuário (entre 1 e 10).  Ex.:
+
+```cs
+Tamanho do retângulo:
+Largura..: 10
+Altura...: 4
+
+**********
+**********
+**********
+**********
+```
